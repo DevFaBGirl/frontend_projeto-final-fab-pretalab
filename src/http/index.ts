12f0a344bf486f177d3b64cb7516f3ed8ec52c3e
Fabiana Balduína <fabiana.silva1@estudante.ifb.edu.br>
2025-01-23 17:11:21 -0300

    :wrench: correção de url do render

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://backend-projeto-final-fab-pretalab.onrender.comm",
+  baseURL: "https://backend-projeto-final-fab-pretalab.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
