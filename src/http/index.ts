2bbce813ac317984546042a279ea6d3b4ed96a8c
Fabiana Balduína <fabiana.silva1@estudante.ifb.edu.br>
2025-01-24 21:28:01 -0300

    :construction: alteração do baseUrl

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://projeto-final-fab-pretalab.vercel.app/dashboard",
+  baseURL: "https://backend-projeto-final-fab-pretalab.onrender.com",
   //baseURL: "http://localhost:3333",
   headers: {
     "Content-Type": "application/json",
